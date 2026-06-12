4b1e863f8f3f8a885bd00f5f69fe2648324e470a
Harini R <harinirajesh0519@gmail.com>
2026-06-12 16:29:22 +0530

    Update Analytics.jsx

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -20,7 +20,7 @@ function Analytics() {
 
       const response =
         await axios.get(
-          "http://127.0.0.1:8000/analytics"
+          "https://cyberintel-ai.onrender.com/analytics"
         );
 
       setData(response.data);
